bfc0b9bd36fd0cdcf63b52fd3ae48d1c524ef264
BenLue <benlue@s3root.ovh>
2017-04-19 14:56:13 +0200

    Temp TWRP Fix: Update Roboto headers to custom font support changes

--- 14.1/jfltexx/Scripts/start_twrp_build.sh
+++ 14.1/jfltexx/Scripts/start_twrp_build.sh
@@ -16,6 +16,12 @@ export USE_CCACHE=1
 
 make clean && make installclean && make clobber
 
+echo ""
+echo "minui: Update Roboto headers to custom font support changes"
+echo "https://gerrit.omnirom.org/#/q/topic:mr2-custom-fonts+(status:open+OR+status:merged)"
+echo ""
+repopick -P bootable/recovery-twrp -g https://gerrit.omnirom.org 22768 22769 22770
+
 CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
 let "CPU_COUNT++"
 OPTIONS="-j $CPU_COUNT"
